916f005e2398c78252ecbe0ba161c9b8d5897212
ciara <cbdoelman@gmail.com>
2022-11-17 16:58:16 +1300

    delete image aspect of the moodboard.js

--- server/db/migrations/moodboard.js
+++ server/db/migrations/moodboard.js
@@ -1,7 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.createTable('moodboard', (table) => {
     table.increments('id').primary()
-    table.string('mood')
     // ^ image url or whatev
     table.string('description')
     table.string('category')
