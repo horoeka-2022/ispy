19d3260aa462262d2762284486d4a72634462441
charanjot <charanjotsingh23@gmail.com>
2022-11-18 10:16:57 +1300

    added server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const path = require('path')
+const moodboard = require('./routes/moodboard')
 
 const server = express()
-
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
+server.use('api/v1/moodboard', moodboard)
+
+server.get('*', (req, res) => {
+  res.sendFile(path.join('public', 'index.html'))
+})
 module.exports = server
