863b89b1ac976d35b2ff6635075b8f57857d0216
charanjot <charanjotsingh23@gmail.com>
2022-11-18 10:52:39 +1300

    added routes,api,connection db

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const server = express()
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.use('api/v1/moodboard', moodboard)
+server.use('/api/v1/moodboard', moodboard)
 
 server.get('*', (req, res) => {
   res.sendFile(path.join('public', 'index.html'))
