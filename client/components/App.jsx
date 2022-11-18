70209735492e637a4d7d84669cfb2cd364f50a6f
daph <daphnejasminesimons@gmail.com>
2022-11-18 15:10:11 +1300

    ac

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import { postMyPostThenGet } from '../slices/moodboard'
-import { useDispatch } from 'react-redux'
+
 import List from './List'
-import request from 'superagent'
 
 function App() {
-  useEffect(() => {}, [])
-  const dispatch = useDispatch()
+  useEffect(() => { }, [])
 
   const [image, setImage] = useState({ preview: '', data: '' })
   const [status, setStatus] = useState('')
-  const [description, setDescription] = useState('')
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -20,9 +16,6 @@ function App() {
       method: 'POST',
       body: formData,
     })
-    const body = await response.json()
-    const id = body.id
-    await request.post('/api/v1/moodboard').send({ description, id })
     if (response) setStatus(response.statusText)
   }
 
@@ -30,43 +23,33 @@ function App() {
     const img = {
       preview: URL.createObjectURL(e.target.files[0]),
       data: e.target.files[0],
-      description: '',
     }
     setImage(img)
   }
 
-  function handleChange(event) {
-    setDescription(() => event.target.value)
-  }
-
   return (
     <>
-      <h1>iSpy</h1>
-      <List />
-      <div className="App">
-        <h1> Add an Image!</h1>
-        {image.preview && (
-          <img
-            src={image.preview}
-            width="100"
-            height="100"
-            alt="imagepreview"
-          />
-        )}
-        <hr></hr>
-        <form onSubmit={handleSubmit}>
-          <input type="file" name="file" onChange={handleFileChange}></input>
-          <input
-            type="text"
-            name="description"
-            onChange={handleChange}
-            value={description}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        {status && <h4>{status}</h4>}
+      <div>
+        <header><img className="logo" src="./images/Logo.png" alt="cool logo" /></header>
+        <List />
+        <div className="App">
+          <h1> Add an Image!</h1>
+          {image.preview && (
+            <img
+              src={image.preview}
+              width="100"
+              height="100"
+              alt="imagepreview"
+            />
+          )}
+          <hr></hr>
+          <form onSubmit={handleSubmit}>
+            <input type="file" name="file" onChange={handleFileChange}></input>
+            <button type="submit">Submit</button>
+          </form>
+          {status && <h4>{status}</h4>}
+        </div>
       </div>
-
       <h2>Looking good!</h2>
     </>
   )
