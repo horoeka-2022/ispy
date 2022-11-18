50a6802c9110c3ad76da9c03daab302fa9668ba8
charanjot <charanjotsingh23@gmail.com>
2022-11-18 15:32:15 +1300

    flip

--- client/components/MyPost.jsx
+++ client/components/MyPost.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
 function MyPost({ myPost }) {
+  // console.log(myPost.img)
+
+  const str = myPost.img
+
+  const imagePath = str.split('/')[4]
+
+  console.log(imagePath)
+
   return (
     <div key={myPost.id}>
       <label>{myPost.description}</label>
+      <img
+        // src="./server/public/uploads/ef32b522738f8ef05a592ab3d9ae5b64.jpeg"
+        src={`/uploads/${imagePath}`}
+        alt="imgnotrendering"
+      />
     </div>
   )
 }
--- server/db/seeds/moodboard.js
+++ server/db/seeds/moodboard.js
@@ -5,41 +5,49 @@ exports.seed = function (knex) {
       return knex('moodboard').insert([
         {
           id: 1,
+
           description: 'sunset pic',
           category: 'sunset',
         },
         {
           id: 2,
+
           description: 'sunset pic',
           category: 'sunset',
         },
         {
           id: 3,
+
           description: 'sunset pic',
           category: 'sunset',
         },
         {
           id: 4,
+
           description: 'sunset pic',
           category: 'sunset',
         },
         {
           id: 5,
+
           description: 'sunset pic',
           category: 'sunset',
         },
         {
           id: 6,
+
           description: 'sunset pic',
           category: 'sunset',
         },
         {
           id: 7,
+
           description: 'sunset pic',
           category: 'sunset',
         },
         {
           id: 8,
+
           description: 'sunset pic',
           category: 'sunset',
         },
